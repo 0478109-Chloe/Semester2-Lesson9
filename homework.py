a0e450c20278f13e1b5b0d34d2fe8cbc72242733
stevengao-ytc <stevengao213@gmail.com>
2026-09-19 14:10:33 +1000

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,19 +1,9 @@
-# In class, we have made a simple turtle program that draws a square.
-# The python file (draw_square.py) for this program is included in the repository for your reference.
-# So the homework is to make a triangle in python turtle module, using the functions we learned in class.
+# Make the minigame three: Rock Paper Scissors
 
-# TODO: edit this file to make a triangle of any colour or size you like
-# Since we use a new module called turtle, regular python compiler websites (like online-python.com) will not work.
-# Please test your script in turtle supported online python environments, like
-# "https://pythonsandbox.com/turtle"
-# or "https://trinket.io/turtle"
-# or "https://stepindev.com/en/py-playground"
+# The computer plays rock paper scissors with you. 
+# The program simulates one turn of rock paper scissors, 
+# and prints if the player wins or loses after the player makes their move.
 
-
-import turtle
-
-
-
-
+import random
 
 
